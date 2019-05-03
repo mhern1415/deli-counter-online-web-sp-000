c2296a93558eef2ab1c147eaa37476245a7b7850
Mark Hernandez <m.hern1415@gmail.com>
2019-05-03 18:47:26 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -8,6 +8,6 @@ end
 
 def take_a_number(katz_deli, name)
   katz_deli.push(name)
-  puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
+  puts "The line is currently #{katz_deli.length}, #{name}." 
 end
 
