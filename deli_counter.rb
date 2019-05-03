f9cefdd05097181718a9e0d17d1e87b9a3bf8c1c
Mark Hernandez <m.hern1415@gmail.com>
2019-05-03 18:50:08 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,6 +3,11 @@ katz_deli = []
 def line(katz_deli)
   if katz_deli.length == 0 
     puts "The line is currently empty."
+  else
+    x.each.with_index(1) do |name, index|
+      line_array.push("#{index}. #{name}")
+    end
+    puts "The line is currently: #{line_array.join(" ")}"
   end
 end
 
